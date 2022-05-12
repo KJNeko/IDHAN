44bb62e2e41d63fd18e7cef3482dbee4b9815838
Kristopher Pearson <bigskyman02@gmail.com>
2022-05-12 07:31:33 -0400

    Main testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,26 @@
-#include "include/Database/config.hpp"
-#include "include/Database/database.hpp"
+#include <string>
+#include <iostream>
 
-#include "include/Utility/HashFilepath.hpp"
+#include "include/IDHAN/Utility/Cache.hpp"
+
+#include "include/IDHAN/DB/Tags.hpp"
+
+
+#include <optional>
+
+std::optional<std::string> getFromDBOrSomething( size_t id )
+{
+
+	return std::nullopt;
+}
+
 
 int main()
 {
+
+	Cache<size_t, std::string> namespaceCache( getFromDBOrSomething );
+
+	auto ret = namespaceCache.get( 0 );
 
 	return 0;
 }
