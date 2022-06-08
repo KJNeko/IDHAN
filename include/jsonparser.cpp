4d05e52ea87c8840220c47295087e7125d082387
Kristopher Pearson <bigskyman02@gmail.com>
2022-06-07 22:01:30 -0400

    Finish up file imports for images

---
+++ include/jsonparser.cpp
@@ -0,0 +1,202 @@
+//
+// Created by kj16609 on 6/7/22.
+//
+
+#include "jsonparser.hpp"
+
+std::string parseJson(std::string& json)
+{
+	auto jsn = nlohmann::json::parse(json);
+	
+	nlohmann::json output;
+	
+	for(auto& item : jsn.items())
+	{
+		//auto operationID = item["operation"].get<OperationType>();
+		
+		std::cout << "Operating on item: " << item.key() << std::endl;
+		
+		auto jsnitem = jsn[item.key()];
+		OperationType opID = jsnitem["operation"].get<OperationType>();
+		switch(opID)
+		{
+			case OperationType::AddFile:
+			{
+				for(auto& file : jsn[item.key()]["filepaths"].items())
+				{
+					std::filesystem::path path = file.value().get<std::string>();
+					if(!std::filesystem::exists(path))
+					{
+						output[item.key()]["failed"][file.key()] = "File does not exist: " + path.string();
+						continue;
+					}
+					
+					//addFile to the database
+					uint64_t hashID = addFile(path);
+					if(hashID == 0)
+					{
+						output[item.key()]["failed"][file.key()] = "File parser was unable to make sense of the file";
+						continue;
+					}
+					
+					output[item.key()]["imported"][file.key()]["filepath"] = path.string();
+					
+					output[item.key()]["imported"][file.key()]["tabledata"] = getFileinfo(hashID);
+					
+				}
+			}
+				break;
+			case OperationType::RemoveFile:
+				std::cout << "STUB REMOVEFILE" << std::endl;
+				break;
+			case OperationType::AddTag: [[fallthrough]];
+			case OperationType::RemoveTag:
+			{
+				//Convert the tags into the propery array we want
+				std::vector<std::pair<std::string, std::string>> tags;
+				
+				for(auto& tag : jsn[item.key()]["tags"])
+				{
+					//Check if the group should be empty or not
+					std::string groupStr{""};
+					if(tag.contains("group"))
+					{
+						groupStr = tag["group"].get<std::string>();
+					}
+					
+					tags.emplace_back(std::make_pair(groupStr, tag["subtag"].get<std::string>()));
+				}
+				
+				auto list = jsn[item.key()]["hashIDs"].get<std::vector<uint64_t>>();
+				
+				std::vector<uint64_t> success, fails;
+				
+				if(opID == OperationType::AddTag)
+				{
+					for(auto& id : list)
+					{
+						try
+						{
+							addTag(id, tags);
+						}
+						catch(std::exception& e)
+						{
+							fails.push_back(id);
+							continue;
+						}
+						success.push_back(id);
+					}
+				}
+				else
+				{
+					for(auto& id : list)
+					{
+						try
+						{
+							removeTag(id, tags);
+						}
+						catch(std::exception& e)
+						{
+							fails.push_back(id);
+							continue;
+						}
+						success.push_back(id);
+					}
+				}
+				
+				if(!success.empty())
+				{
+					output[item.key()]["succeeded"] = success;
+				}
+				
+				if(!fails.empty())
+				{
+					output[item.key()]["failed"] = fails;
+				}
+				break;
+			}
+			case OperationType::GetTag:
+			{
+				auto hashID = jsn[item.key()]["hashIDs"].get<std::vector<uint64_t>>();
+				
+				for ( auto& id : hashID )
+				{
+					try
+					{
+						
+						std::vector<std::pair<std::string, std::string>> tags = getTags( id );
+						
+						std::vector<std::string> tagConcant;
+						
+						for ( auto& [group, subtag] : tags )
+						{
+							if ( group == "" )
+							{
+								tagConcant.emplace_back( subtag );
+							}
+							else
+							{
+								tagConcant.emplace_back( group + ":" + subtag );
+							}
+						}
+						
+						output[item.key()]["succeeded"][std::to_string( id )] = tagConcant;
+					}
+					catch(std::exception& e)
+					{
+						output[item.key()]["failed"][std::to_string( id )] = e.what();
+					}
+				}
+				
+				break;
+			}
+			
+			case OperationType::RenameTag:
+			{
+				std::vector<uint64_t> failedVec, succeededVec;
+				
+				for(auto [key, value] : jsn[item.key()]["pairs"].items())
+				{
+					auto ref = jsn[item.key()]["pairs"][key];
+					
+					auto origin = ref["origin"];
+					auto replacement = ref["new"];
+					
+					std::string originGroup {""};
+					if(origin.contains("group"))
+					{
+						originGroup = origin["group"].get<std::string>();
+					}
+					std::string originSubtag{origin["subtag"].get<std::string>()};
+					
+					
+					
+					std::string replaceGroup {""};
+					if(replacement.contains("group"))
+					{
+						replaceGroup = replacement["group"].get<std::string>();
+					}
+					std::string replaceSubtag{replacement["subtag"].get<std::string>()};
+					
+					replaceTag(std::make_pair(originGroup, originSubtag), std::make_pair(replaceGroup, replaceSubtag));
+					
+					succeededVec.push_back(std::stoull(key));
+				}
+				
+				if(!succeededVec.empty())
+				{
+					output[item.key()]["succeeded"] = succeededVec;
+				}
+				
+				if(!failedVec.empty())
+				{
+					output[item.key()]["failed"] = failedVec;
+				}
+			}
+				break;
+		}
+		
+	}
+	
+	return output.dump();
+}
--- src/databaseTests.cpp
+++ src/databaseTests.cpp
@@ -77,17 +77,32 @@ query:SELECT groupid, subtagid FROM tags NATURAL JOIN mappings WHERE hashid IN (
 
 #include "database.hpp"
 #include "jsonparser.hpp"
+#include "crypto.hpp"
 
 
 #define CONSTEXPR_REQUIRES(expr) \
   static_assert(expr);\
   REQUIRE(expr);
 
-
 void resetDB()
 {
 	Connection conn;
 	conn.resetDB();
+}
+
+TEST_CASE("SHA256Test", "[crypto]")
+{
+	std::vector<uint8_t> expected {0x9f,0x86,0xd0,0x81,0x88,0x4c,0x7d,0x65,0x9a,0x2f,0xea,0xa0,0xc5,0x5a,0xd0,0x15,0xa3,0xbf,0x4f,0x1b,0x2b,0x0b,0x82,0x2c,0xd1,0x5d,0x6c,0x15,0xb0,0xf0,0x0a,0x08};
+	std::vector<uint8_t> data = {'t','e','s','t'};
+	REQUIRE(SHA256(data) == expected);
+}
+
+TEST_CASE("MD5Test", "[crypto]")
+{
+	std::vector<uint8_t> expected {0x09,0x8f,0x6b,0xcd,0x46,0x21,0xd3,0x73,0xca,0xde,0x4e,0x83,0x26,0x27,0xb4,0xf6};
+	std::vector<uint8_t> data = {'t','e','s','t'};
+	
+	REQUIRE(MD5(data) == expected);
 }
 
 TEST_CASE("ConnectionTest", "[database]")
@@ -98,6 +113,7 @@ TEST_CASE("ConnectionTest", "[database]")
 TEST_CASE("addTag", "[tags][database]")
 {
 	resetDB();
+	
 	std::vector<std::pair<std::string, std::string>> tags;
 	tags.push_back(std::make_pair("character", "toujou koneko"));
 	tags.push_back(std::make_pair("series", "Highschool DxD"));
@@ -105,7 +121,12 @@ TEST_CASE("addTag", "[tags][database]")
 	
 	//First paramter: HashID
 	//Second paramter: List of tags to add
-	REQUIRE(addTag(1, tags) == true);
+	uint64_t id = addFile("./../Images/valid.jpg");
+	if(id == 0)
+	{
+		throw std::runtime_error("Could not add file");
+	}
+	addTag(id, tags);
 	
 	//Testing
 	{
@@ -131,11 +152,14 @@ TEST_CASE("addTag", "[tags][database]")
 		
 		wrk.commit();
 	}
+	
+	resetDB();
 }
 
 TEST_CASE("removeTag", "[tags][database]")
 {
 	resetDB();
+	
 	std::vector<std::pair<std::string, std::string>> tags;
 	tags.push_back(std::make_pair("character", "toujou koneko"));
 	tags.push_back(std::make_pair("series", "Highschool DxD"));
@@ -143,8 +167,13 @@ TEST_CASE("removeTag", "[tags][database]")
 	
 	//First paramter: HashID
 	//Second paramter: List of tags to add
-	addTag(1, tags);
-	removeTag(1, tags);
+	uint64_t id = addFile("./../Images/valid.jpg");
+	if(id == 0)
+	{
+		throw std::runtime_error("Could not add file");
+	}
+	addTag(id, tags);
+	removeTag(id, tags);
 	
 	
 	{
@@ -163,11 +192,13 @@ TEST_CASE("removeTag", "[tags][database]")
 		wrk.commit();
 	}
 	
+	resetDB();
 }
 
 TEST_CASE("getTags", "[tags][database]")
 {
 	resetDB();
+	
 	std::vector<std::pair<std::string, std::string>> tags;
 	tags.push_back(std::make_pair("character", "toujou koneko"));
 	tags.push_back(std::make_pair("series", "Highschool DxD"));
@@ -175,34 +206,56 @@ TEST_CASE("getTags", "[tags][database]")
 	
 	//First paramter: HashID
 	//Second paramter: List of tags to add
-	addTag(1, tags);
+	uint64_t id = addFile("./../Images/valid.jpg");
+	if(id == 0)
+	{
+		throw std::runtime_error("Could not add file");
+	}
+	addTag(id, tags);
 	
 	auto retTags = getTags(1);
 	REQUIRE(retTags.size() == tags.size());
 	REQUIRE(retTags == tags);
+	
+	resetDB();
 }
 
 TEST_CASE("jsonParseAddFile", "[json][database]")
 {
+	resetDB();
+	
 	std::string jsonStr = R"(
 	{
 		"0": {
 			"operation": 0,
 			"filepaths": {
-				"0": "/test/"
+				"0": "/home/kj16609/Desktop/Projects/IDHAN/Images/valid.jpg",
+				"1": "/home/kj16609/Desktop/Projects/IDHAN/Images/doesntexist.png",
+				"2": "/home/kj16609/Desktop/Projects/IDHAN/Images/invalid.txt"
 			}
 		}
 	}
 	)";
 	
-	std::string expected = R"({"1":{"succeeded":[1,2,3,4]}})";
+	std::string expected = R"({"0":{"failed":{"1":"File does not exist: /home/kj16609/Desktop/Projects/IDHAN/Images/doesntexist.png","2":"File parser was unable to make sense of the file"},"imported":{"0":{"filepath":"/home/kj16609/Desktop/Projects/IDHAN/Images/valid.jpg","tabledata":{"importinfo":{"filename":"valid.jpg","time":"2022-06-07 20:41:47.789093"},"mappings":[],"playerinfo":{"bytes":2556576,"duration":0,"fps":0,"frames":1,"height":1960,"type":1,"width":4032}}}}}})";
 	
-	REQUIRE(parseJson(jsonStr) == expected);
+	//Check for fail condition
+	std::string data = parseJson(jsonStr);
+	
+	//Check if the first 25 characters match
+	REQUIRE(data.substr(0, 301) == expected.substr(0, 301));
+	
+	//Check if the last 121 characters match
+	REQUIRE(data.substr(data.size() - 120, 121) == expected.substr(expected.size() - 120, 121));
+	
+	
 	resetDB();
 }
 
 TEST_CASE("jsonParseRemoveFile", "[json][database]")
 {
+	resetDB();
+	
 	std::string jsonStr = R"(
 	{
 		"1": {
@@ -214,17 +267,30 @@ TEST_CASE("jsonParseRemoveFile", "[json][database]")
 	
 	std::string expected = R"({"1":{"succeeded":[1,2,3,4]}})";
 	
-	REQUIRE(parseJson(jsonStr) == expected);
+	auto data = parseJson(jsonStr);
+	
+	std::cout << data << std::endl;
+	std::cout << expected << std::endl;
+	
+	REQUIRE(data == expected);
 	resetDB();
 }
 
 TEST_CASE("jsonParseAddTag", "[json][database]")
 {
+	resetDB();
+	
 	std::string jsonStr = R"(
 	{
+		"0": {
+			"operation": 0,
+			"filepaths": {
+				"0": "/home/kj16609/Desktop/Projects/IDHAN/Images/valid.jpg"
+			}
+		},
 		"2": {
 			"operation": 2,
-			"hashIDs": [1,2,3,4],
+			"hashIDs": [1,2],
 			"tags": {
 				"0": {
 					"group": "",
@@ -239,19 +305,37 @@ TEST_CASE("jsonParseAddTag", "[json][database]")
 	}
 	)";
 	
-	std::string expected = R"({"2":{"succeeded":[1,2,3,4]}})";
+	std::string expected = R"({"0":{"imported":{"0":{"filepath":"/home/kj16609/Desktop/Projects/IDHAN/Images/valid.jpg","tabledata":{"importinfo":{"filename":"valid.jpg","time":"2022-06-07 20:50:42.272076"},"mappings":[],"playerinfo":{"bytes":2556576,"duration":0,"fps":0,"frames":1,"height":1960,"type":1,"width":4032}}}}},"2":{"failed":[2],"succeeded":[1]}})";
 	
-	REQUIRE(parseJson(jsonStr) == expected);
+	auto data = parseJson(jsonStr);
+	
+	std::cout << data << std::endl;
+	std::cout << expected << std::endl;
+	
+	//Check that the first 151 characters match
+	REQUIRE(data.substr(0, 149) == expected.substr(0, 149));
+	
+	//Check that the last 160 characters match
+	REQUIRE(data.substr(data.size() - 154, 155) == expected.substr(expected.size() - 154, 155));
+	
 	resetDB();
 }
 
 TEST_CASE("jsonParseRemoveTag", "[json][database]")
 {
+	resetDB();
+	
 	std::string jsonStr = R"(
 	{
+		"0": {
+			"operation": 0,
+			"filepaths": {
+				"0": "/home/kj16609/Desktop/Projects/IDHAN/Images/valid.jpg"
+			}
+		},
 		"2": {
 			"operation": 2,
-			"hashIDs": [1,2,3,4],
+			"hashIDs": [1],
 			"tags": {
 				"0": {
 					"group": "",
@@ -276,19 +360,34 @@ TEST_CASE("jsonParseRemoveTag", "[json][database]")
 	}
 	)";
 	
-	std::string expected = R"({"2":{"succeeded":[1,2,3,4]},"3":{"succeeded":[1,2,3,4]}})";
+	std::string expected = R"({"0":{"imported":{"0":{"filepath":"/home/kj16609/Desktop/Projects/IDHAN/Images/valid.jpg","tabledata":{"importinfo":{"filename":"valid.jpg","time":"2022-06-07 21:57:41.678387"},"mappings":[],"playerinfo":{"bytes":2556576,"duration":0,"fps":0,"frames":1,"height":1960,"type":1,"width":4032}}}}},"2":{"succeeded":[1]},"3":{"succeeded":[1,2,3,4]}})";
 	
-	REQUIRE(parseJson(jsonStr) == expected);
+	auto data = parseJson(jsonStr);
+	
+	//Compare the first 148 characters
+	REQUIRE(data.substr(0, 148) == expected.substr(0, 148));
+	
+	//Compare the last 170 characters
+	REQUIRE(data.substr(data.size() - 170, 171) == expected.substr(expected.size() - 170, 171));
+	
 	resetDB();
 }
 
 TEST_CASE("jsonParseGetTag", "[json][database]")
 {
+	resetDB();
+	
 	std::string jsonStr = R"(
 	{
+	"0": {
+		"operation": 0,
+		"filepaths": {
+			"0": "/home/kj16609/Desktop/Projects/IDHAN/Images/valid.jpg"
+		}
+	},
 	"2": {
 		"operation": 2,
-		"hashIDs": [1,2,3,4],
+		"hashIDs": [1],
 		"tags": {
 			"0": {
 				"group": "",
@@ -302,62 +401,101 @@ TEST_CASE("jsonParseGetTag", "[json][database]")
 	},
 	"7": {
 		"operation": 4,
-		"hashIDs": [1,2,3,4]
+		"hashIDs": [1,2]
 	}
 	}
 	)";
 	
-	std::string expected = R"({"2":{"succeeded":[1,2,3,4]},"7":{"1":["toujou koneko","series:Highschool DxD"],"2":["toujou koneko","series:Highschool DxD"],"3":["toujou koneko","series:Highschool DxD"],"4":["toujou koneko","series:Highschool DxD"]}})";
+	std::string expected = R"({"0":{"imported":{"0":{"filepath":"/home/kj16609/Desktop/Projects/IDHAN/Images/valid.jpg","tabledata":{"importinfo":{"filename":"valid.jpg","time":"2022-06-07 21:10:52.871199"},"mappings":[],"playerinfo":{"bytes":2556576,"duration":0,"fps":0,"frames":1,"height":1960,"type":1,"width":4032}}}}},"2":{"succeeded":[1]},"7":{"failed":{"2":"HashID does not exist"},"succeeded":{"1":["toujou koneko","series:Highschool DxD"]}}})";
 	
-	REQUIRE(parseJson(jsonStr) == expected);
+	auto data = parseJson(jsonStr);
+	
+	
+	
+	//Check that the first 148 characters match
+	REQUIRE(data.substr(0, 148) == expected.substr(0, 148));
+	
+	//Check that the last 247 characters match
+	REQUIRE(data.substr(data.size() - 247, 248) == expected.substr(expected.size() - 247, 248));
+	
 	resetDB();
 }
 
 TEST_CASE("jsonParseRenameTag", "[json][database]")
 {
+	resetDB();
+	
 	std::string jsonStr = R"(
 	{
-	"2": {
-		"operation": 2,
-		"hashIDs": [1,2,3,4],
-		"tags": {
-			"0": {
-				"group": "",
-				"subtag": "toujou koneko"
-			},
-			"1": {
-				"group": "series",
-				"subtag": "Highschool DxD"
+		"0": {
+			"operation": 0,
+			"filepaths": {
+				"0": "/home/kj16609/Desktop/Projects/IDHAN/Images/valid.jpg"
 			}
-		}
-	},
-	"5": {
-		"operation": 5,
-		"pairs": {
-			"0": {
-				"origin": {
+		},
+		"2": {
+			"operation": 2,
+			"hashIDs": [
+				1
+			],
+			"tags": {
+				"0": {
 					"group": "",
 					"subtag": "toujou koneko"
 				},
-				"new": {
-					"group": "character",
-					"subtag": "toujou koneko"
-				}
+				"1": {
+					"group": "series",
+					"subtag": "Highschool DxD"
 				}
 			}
+		},
+		"4": {
+			"operation": 4,
+			"hashIDs": [
+				1
+			]
+		},
+		"5": {
+			"operation": 5,
+			"pairs": {
+				"0": {
+					"origin": {
+						"group": "",
+						"subtag": "toujou koneko"
+					},
+					"new": {
+						"group": "character",
+						"subtag": "toujou koneko"
+					}
+				}
+			}
+		},
+		"6": {
+			"operation": 4,
+			"hashIDs": [
+				1
+			]
 		}
 	}
 	)";
 	
-	std::string expected = R"({"2":{"succeeded":[1,2,3,4]},"5":{"succeeded":[0]}})";
+	std::string expected = R"({"0":{"imported":{"0":{"filepath":"/home/kj16609/Desktop/Projects/IDHAN/Images/valid.jpg","tabledata":{"importinfo":{"filename":"valid.jpg","time":"2022-06-07 21:53:53.418013"},"mappings":[],"playerinfo":{"bytes":2556576,"duration":0,"fps":0,"frames":1,"height":1960,"type":1,"width":4032}}}}},"2":{"succeeded":[1]},"4":{"succeeded":{"1":["toujou koneko","series:Highschool DxD"]}},"5":{"succeeded":[0]},"6":{"succeeded":{"1":["series:Highschool DxD","character:toujou koneko"]}}})";
 	
-	REQUIRE(parseJson(jsonStr) == expected);
+	auto data = parseJson(jsonStr);
+	
+	//Check the first 148 characters match
+	REQUIRE(data.substr(0, 148) == expected.substr(0, 148));
+	
+	//Check the last 306 characters
+	REQUIRE(data.substr(data.size() - 306, 307) == expected.substr(expected.size() - 306, 307));
+	
 	resetDB();
 }
 
 TEST_CASE("jsonParse", "[json][database]")
 {
 	resetDB();
+	
 	std::string jsonStr = R"(
 	{
 		"0": {
@@ -438,5 +576,6 @@ TEST_CASE("jsonParse", "[json][database]")
 	
 	std::cout << parseJson(jsonStr) << std::endl;
 	REQUIRE(parseJson(jsonStr) == expected);
+	
 	resetDB();
 }
