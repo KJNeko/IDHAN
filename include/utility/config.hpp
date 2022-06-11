b6512c82feefe6f193952c6e690277ac076c962d
Kristopher Pearson <bigskyman02@gmail.com>
2022-06-11 04:05:21 -0400

    Extra config options to support some tests

--- include/utility/config.hpp
+++ include/utility/config.hpp
@@ -17,16 +17,18 @@ namespace idhan
 		
 		inline static std::mutex configLock;
 		
+		
+		//Import and file configs
 		inline static std::filesystem::path thumbnail_path { "./db/thumbnails/" };
-		inline static std::atomic<bool> thumbnail_pathValid = false;
+		inline static std::atomic<bool> thumbnail_pathValid { false };
 		
 		inline static std::filesystem::path file_path { "./db/file/"};
-		
 		
 		//vips
 		inline static std::atomic<bool> vips_allow_threaded;
 		
 		
+		inline static std::atomic<bool> debug { false };
 	};
 }
 
