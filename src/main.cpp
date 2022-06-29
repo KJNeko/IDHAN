dc281bebd2349a3035f9963f56931fcbb46c4f12
KJ16609 <bigskyman02@gmail.com>
2022-06-29 10:20:19 -0400

    Fixes windows build system

--- src/main.cpp
+++ src/main.cpp
@@ -11,33 +11,27 @@
 
 #include "TracyBox.hpp"
 
-#include "database.hpp"
-#include "database/idhanDatabase.hpp"
-
 int main( int argc, char** argv )
 {
+	try
 	{
-		Database db;
-		db.init();
+		std::cout << "Running" << std::endl;
+		
+		std::cout << "Qt version: " << qVersion() << std::endl;
+		
+		QApplication app( argc, argv );
+		
+		MainWindow window;
+		window.show();
+		
+		return app.exec();
 	}
-
-	Hash hash;
-
-
-	const QByteArray hash_var = QByteArray::fromRawData( (const char*)hash.data(), hash.size() );
-	const QString hash_hex	  = hash_var.toHex();
-	const std::string view	  = hash_hex.toStdString();
-
-	std::cout << view << std::endl;
-
-	std::cout << "Id: " << getFileID( hash, true ) << std::endl;
-
-	std::cout << "Qt version: " << qVersion() << std::endl;
-
-	QApplication app( argc, argv );
-
-	MainWindow window;
-	window.show();
-
-	return app.exec();
+	catch(std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	catch(...)
+	{
+		std::cout << "wtf" << std::endl;
+	}
 }
