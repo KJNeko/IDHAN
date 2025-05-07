2215ad1162d1d78ea164e19ec8a5cea536caab72
kj16609 <bigskyman02@gmail.com>
2025-05-07 12:23:12 -0400

    Fixes incorrect document response for '/records/{record_id}/tags'

--- IDHANServer/src/api/record/tags/listTags.cpp
+++ IDHANServer/src/api/record/tags/listTags.cpp
@@ -9,6 +9,40 @@ namespace idhan::api
 
 drogon::Task< drogon::HttpResponsePtr > IDHANRecordAPI::
 	listTags( const drogon::HttpRequestPtr request, const RecordID record_id )
-{}
+{
+	const auto db { drogon::app().getDbClient() };
+
+	const auto result {
+		co_await db->execSqlCoro( "SELECT domain_id, tag_id FROM tag_mappings WHERE record_id = $1", record_id )
+	};
+
+	Json::Value json {};
+
+	std::unordered_map< TagDomainID, Json::ArrayIndex > domain_map {};
+
+	for ( const auto& row : result )
+	{
+		const auto domain_id { row[ 0 ].as< std::size_t >() };
+		const auto tag_id { row[ 1 ].as< std::size_t >() };
+
+		if ( auto itter = domain_map.find( domain_id ); itter != domain_map.end() )
+		{
+			json[ itter->second ][ "tag_ids" ].append( tag_id );
+			continue;
+		}
+		else
+		{
+			Json::Value obj {};
+			obj[ "domain_id" ] = domain_id;
+			obj[ "tag_ids" ] = Json::arrayValue;
+			obj[ "tag_ids" ].append( tag_id );
+			json.append( obj );
+
+			domain_map[ domain_id ] = json.size() - 1;
+		}
+	}
+
+	co_return drogon::HttpResponse::newHttpJsonResponse( json );
+}
 
 } // namespace idhan::api
