bec85ea46707ae05298f25e8bfafd0a4d71ffe36
kj16609 <bigskyman02@gmail.com>
2025-07-16 02:02:28 -0400

    Enable `IDHAN_USE_STD_FORMAT` and disable testing in Finddrogon configuration

--- dependencies/spdlogConfig.cmake
+++ dependencies/spdlogConfig.cmake
@@ -4,6 +4,7 @@ if (NOT TARGET spdlog)
 	set(CMAKE_CXX_STANDARD 23)
 	set(CMAKE_CXX_STANDARD_REQUIRED ON)
 	set(SPDLOG_USE_STD_FORMAT ON)
+	set(IDHAN_USE_STD_FORMAT ON)
 
 	add_subdirectory(${CMAKE_SOURCE_DIR}/dependencies/spdlog ${CMAKE_CURRENT_BINARY_DIR}/spdlog)
 endif ()
