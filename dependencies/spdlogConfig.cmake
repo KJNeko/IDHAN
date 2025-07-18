080815169d5d61ff2c80fb9b5085aff72c8343ea
kj16609 <bigskyman02@gmail.com>
2025-07-17 22:41:41 -0400

    Enable std::format usage where possible

--- dependencies/spdlogConfig.cmake
+++ dependencies/spdlogConfig.cmake
@@ -1,10 +1,10 @@
 
+set(SPDLOG_USE_STD_FORMAT ON)
 
 if (NOT TARGET spdlog)
 	set(CMAKE_CXX_STANDARD 23)
 	set(CMAKE_CXX_STANDARD_REQUIRED ON)
 	set(SPDLOG_USE_STD_FORMAT ON)
-	set(IDHAN_USE_STD_FORMAT ON)
 
 	add_subdirectory(${CMAKE_SOURCE_DIR}/dependencies/spdlog ${CMAKE_CURRENT_BINARY_DIR}/spdlog)
 endif ()
