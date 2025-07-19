c3f000419d3c895d49f6e54d608502dd78bdd5a9
kj16609 <bigskyman02@gmail.com>
2025-07-19 03:13:15 -0400

    Redirect libVIPS logging to spdlog

--- IDHANModules/premade/entry.cpp
+++ IDHANModules/premade/entry.cpp
@@ -2,8 +2,11 @@
 // Created by kj16609 on 6/11/25.
 //
 
+#include <spdlog/spdlog.h>
 #include <vips/vips.h>
 
+#include <format>
+#include <functional>
 #include <memory>
 #include <vector>
 
@@ -36,9 +39,50 @@ void* getModulesFunc()
 	return reinterpret_cast< void* >( &getModules );
 }
 
+void gLoghandler(
+	[[maybe_unused]] const char* log_domain,
+	GLogLevelFlags log_level,
+	const char* message,
+	[[maybe_unused]] gpointer user_data )
+{
+	switch ( log_level )
+	{
+		case G_LOG_LEVEL_MASK:
+			[[fallthrough]];
+		default:
+			[[fallthrough]];
+		case G_LOG_FLAG_RECURSION:
+			[[fallthrough]];
+		case G_LOG_FLAG_FATAL:
+			[[fallthrough]];
+		case G_LOG_LEVEL_CRITICAL:
+			spdlog::critical( std::format( "VIPS: {}", message ) );
+			break;
+		case G_LOG_LEVEL_ERROR:
+			spdlog::error( std::format( "VIPS: {}", message ) );
+			break;
+		case G_LOG_LEVEL_WARNING:
+			spdlog::warn( std::format( "VIPS: {}", message ) );
+			break;
+		case G_LOG_LEVEL_INFO:
+			[[fallthrough]];
+		case G_LOG_LEVEL_MESSAGE:
+			// Very noisy
+			// spdlog::info( std::format( "VIPS: {}", message ) );
+			break;
+		case G_LOG_LEVEL_DEBUG:
+			spdlog::debug( std::format( "VIPS: {}", message ) );
+	}
+}
+
 // Defined and implemented if a library used by these modules must be initalized first
 void init()
 {
+	// Take over the libVIPS logger
+
+	constexpr auto VIPS_LOG_DOMAIN { "VIPS" };
+	g_log_set_handler( VIPS_LOG_DOMAIN, G_LOG_LEVEL_MASK, &gLoghandler, nullptr );
+	g_logv( VIPS_LOG_DOMAIN, G_LOG_LEVEL_INFO, "VIPS Logger taken over", nullptr );
 	VIPS_INIT( "IDHANPremadeModules" );
 }
 
