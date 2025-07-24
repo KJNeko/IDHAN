0437c6d142610e0cffeab34b31b2684814a8c4a6
kj16609 <bigskyman02@gmail.com>
2025-07-24 03:39:40 -0400

    Fixes color constraint for tag_namespaces

--- IDHANServer/src/db/setup/migration/15-tag_namespaces.sql
+++ IDHANServer/src/db/setup/migration/15-tag_namespaces.sql
@@ -3,5 +3,5 @@ CREATE TABLE tag_namespaces
     namespace_id   SERIAL PRIMARY KEY,
     namespace_text TEXT UNIQUE NOT NULL,
     color          bytea,
-    CHECK (color IS NULL OR OCTET_LENGTH(color) == 3)
+    CHECK (color IS NULL OR OCTET_LENGTH(color) = 3)
 );
