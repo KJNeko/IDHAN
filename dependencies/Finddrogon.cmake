ba0084884623dd7e35b6de84d9b73d577387a7c1
kj16609 <bigskyman02@gmail.com>
2025-07-25 02:05:35 -0400

    Mark drogon's headers as system includes to prevent warning spam

--- dependencies/Finddrogon.cmake
+++ dependencies/Finddrogon.cmake
@@ -17,3 +17,10 @@ set(USE_SUBMODULE OFF)
 
 add_subdirectory(${CMAKE_SOURCE_DIR}/dependencies/drogon ${CMAKE_CURRENT_BINARY_DIR}/drogon)
 set(drogon_FOUND TRUE)
+
+
+# In order to silence a lot of warnings from our flags, we need to mark all drogon's includes as system headers
+get_target_property(DROGON_INCLUDE_DIRS drogon INTERFACE_INCLUDE_DIRECTORIES)
+if (DROGON_INCLUDE_DIRS)
+	target_include_directories(drogon SYSTEM INTERFACE ${DROGON_INCLUDE_DIRS})
+endif ()
