d81bc3d837007f8ebccfabf50075541dc1835a48
kj16609 <bigskyman02@gmail.com>
2025-07-25 02:07:34 -0400

    Silence warnings from libpqxx

--- dependencies/postgres.cmake
+++ dependencies/postgres.cmake
@@ -1,1 +1,6 @@
 add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/dependencies/libpqxx)
+
+get_target_property(PQXX_INCLUDE_DIRS pqxx INTERFACE_INCLUDE_DIRECTORIES)
+if (PQXX_INCLUDE_DIRS)
+	target_include_directories(pqxx SYSTEM INTERFACE ${PQXX_INCLUDE_DIRS})
+endif ()
