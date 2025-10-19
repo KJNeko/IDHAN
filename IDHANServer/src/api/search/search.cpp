e4a3409835b71f8363819ccc49ef38e068946e44
kj16609 <bigskyman02@gmail.com>
2025-10-19 06:53:33 -0400

    Switches IDHAN tag search to use SearchBuilder

--- IDHANServer/src/api/search/search.cpp
+++ IDHANServer/src/api/search/search.cpp
@@ -6,6 +6,7 @@
 
 #include "api/SearchAPI.hpp"
 #include "api/helpers/getArrayParameters.hpp"
+#include "core/search/SearchBuilder.hpp"
 #include "db/TagSearch.hpp"
 #include "logging/log.hpp"
 
@@ -18,36 +19,21 @@ drogon::Task< drogon::HttpResponsePtr > SearchAPI::search( drogon::HttpRequestPt
 
 	// Drogon does not support tag_id=1?tag_id=2 for some reason, But it's possible to be sent like that, So we'll handle it here.
 	// Support both tag_id (singular) and tag_ids (plural) for compatibility
-	auto tag_ids { parseArrayParmeters< std::size_t >( request, "tag_ids" ) };
+	std::vector< TagID > tag_ids { parseArrayParmeters< TagID >( request, "tag_ids" ) };
 	if ( tag_ids.empty() )
 	{
-		tag_ids = parseArrayParmeters< std::size_t >( request, "tag_id" );
+		tag_ids = parseArrayParmeters< TagID >( request, "tag_id" );
 	}
+
 	const auto tag_domain_ids { parseArrayParmeters< TagDomainID >( request, "tag_domains" ) };
 
-	const bool use_stored { request->getOptionalParameter< bool >( "use_stored" ).value_or( false ) };
+	// const bool use_stored { request->getOptionalParameter< bool >( "use_stored" ).value_or( false ) };
 
-	std::string query {
-		"SELECT record_id, count(*) AS count FROM active_tag_mappings WHERE tag_id = ANY($1) and tag_domain_id = ANY($2) GROUP BY record_id, tag_domain_id"
-	};
+	SearchBuilder builder {};
 
-	std::string table_source {};
-	if ( use_stored )
-		table_source = "active_tag_mappings";
-	else
-		table_source = "active_tag_mappings_final";
+	builder.setTags( tag_ids );
 
-	// remove the filter for the domain_id as it's not needed here, we want all domains
-	if ( tag_domain_ids.empty() )
-	{
-		query =
-			"SELECT record_id, count(*) as count FROM active_tag_mappings WHERE tag_id = ANY($1) GROUP BY record_id, tag_domain_id";
-	}
-
-	log::info( "Query: {}", query );
-
-	//TODO: Search for multiple tags
-	const auto result { co_await db->execSqlCoro( query, static_cast< TagID >( tag_ids[ 0 ] ) ) };
+	const auto result { co_await builder.query( db, tag_domain_ids ) };
 
 	if ( result.empty() )
 	{
