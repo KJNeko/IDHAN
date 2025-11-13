d98cc8353937c652b8cb84e753efa823b24abaa8
kj16609 <bigskyman02@gmail.com>
2025-11-13 15:27:35 -0500

    Removes unused function

--- IDHAN/include/hydrus_constants.hpp
+++ IDHAN/include/hydrus_constants.hpp
@@ -15,29 +15,6 @@ enum ServiceTypes
 	TAG_SERVICE = gen_constants::LOCAL_TAG
 };
 
-inline std::uint16_t simpleToHyType( SimpleMimeType type )
-{
-	switch ( type )
-	{
-		default:
-			[[fallthrough]];
-		case SimpleMimeType::NONE:
-			[[fallthrough]];
-		case SimpleMimeType::IMAGE:
-			return gen_constants::GENERAL_IMAGE;
-		case SimpleMimeType::VIDEO:
-			return gen_constants::GENERAL_VIDEO;
-		case SimpleMimeType::ANIMATION:
-			return gen_constants::GENERAL_ANIMATION;
-		case SimpleMimeType::AUDIO:
-			return gen_constants::GENERAL_AUDIO;
-		case SimpleMimeType::ARCHIVE:
-			return gen_constants::GENERAL_APPLICATION_ARCHIVE;
-		case SimpleMimeType::IMAGE_PROJECT:
-			return gen_constants::GENERAL_IMAGE_PROJECT;
-	}
-}
-
 inline std::uint16_t mimeToHyType( const std::string& mime_name )
 {
 	if ( auto itter = hy_type_mime.find( mime_name ); itter != hy_type_mime.end() ) return itter->second;
