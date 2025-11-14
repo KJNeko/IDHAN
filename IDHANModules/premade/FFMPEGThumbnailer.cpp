d130a9bde3f5a93b38a34f66ce4e3f9fea7cfcd5
kj16609 <bigskyman02@gmail.com>
2025-11-14 00:58:02 -0500

    Fixes data alignment creating invalid thumbnails

--- IDHANModules/premade/FFMPEGThumbnailer.cpp
+++ IDHANModules/premade/FFMPEGThumbnailer.cpp
@@ -20,6 +20,85 @@ extern "C" {
 #include <libswscale/swscale.h>
 }
 
+// Custom deleters for FFmpeg types
+struct AVFormatContextDeleter
+{
+	void operator()( AVFormatContext* ctx ) const
+	{
+		if ( ctx )
+		{
+			avformat_close_input( &ctx );
+			avformat_free_context( ctx );
+		}
+	}
+};
+
+struct AVCodecContextDeleter
+{
+	void operator()( AVCodecContext* ctx ) const
+	{
+		if ( ctx )
+		{
+			avcodec_free_context( &ctx );
+		}
+	}
+};
+
+struct AVPacketDeleter
+{
+	void operator()( AVPacket* pkt ) const
+	{
+		if ( pkt )
+		{
+			av_packet_free( &pkt );
+		}
+	}
+};
+
+struct AVFrameDeleter
+{
+	void operator()( AVFrame* frame ) const
+	{
+		if ( frame )
+		{
+			av_frame_free( &frame );
+		}
+	}
+};
+
+struct SwsContextDeleter
+{
+	void operator()( SwsContext* ctx ) const
+	{
+		if ( ctx )
+		{
+			sws_freeContext( ctx );
+		}
+	}
+};
+
+struct AVIOContextDeleter
+{
+	void operator()( AVIOContext* ctx ) const
+	{
+		if ( ctx )
+		{
+			av_free( ctx );
+		}
+	}
+};
+
+struct VipsImageDeleter
+{
+	void operator()( VipsImage* img ) const
+	{
+		if ( img )
+		{
+			g_object_unref( img );
+		}
+	}
+};
+
 std::string_view FFMPEGThumbnailer::name()
 {
 	return "FFMPEGThumbnailer";
@@ -47,37 +126,39 @@ std::expected< idhan::ThumbnailerModuleI::ThumbnailInfo, idhan::ModuleError > FF
 	constexpr auto BUFFER_SIZE { 4096 };
 	std::byte* buffer_ptr { new std::byte[ BUFFER_SIZE ] };
 
-	const std::shared_ptr< AVIOContext > avio_context(
-		avio_alloc_context(
-			reinterpret_cast< unsigned char* >( buffer_ptr ),
-			BUFFER_SIZE,
-			0,
-			&opaque_info,
-			&readFunction,
-			nullptr,
-			seekFunction ),
-		[ buffer_ptr ]( AVIOContext* ctx )
-		{
-			if ( ctx )
-			{
-				av_free( ctx );
-			}
-			delete[] buffer_ptr;
-		} );
+	std::unique_ptr< AVIOContext, AVIOContextDeleter > avio_context( avio_alloc_context(
+		reinterpret_cast< unsigned char* >( buffer_ptr ),
+		BUFFER_SIZE,
+		0,
+		&opaque_info,
+		&readFunction,
+		nullptr,
+		seekFunction ) );
 
-	const auto format_context_p =
-		std::shared_ptr< AVFormatContext >( avformat_alloc_context(), &avformat_free_context );
-	auto format_context { format_context_p.get() };
+	if ( !avio_context )
+	{
+		return std::unexpected( idhan::ModuleError( "Failed to allocate AVIO context" ) );
+	}
+
+	std::unique_ptr< AVFormatContext, AVFormatContextDeleter > format_context( avformat_alloc_context() );
+	if ( !format_context )
+	{
+		return std::unexpected( idhan::ModuleError( "Failed to allocate format context" ) );
+	}
 
 	format_context->pb = avio_context.get();
 	format_context->flags |= AVFMT_FLAG_CUSTOM_IO;
 
-	if ( avformat_open_input( &format_context, "", nullptr, nullptr ) < 0 )
+	AVFormatContext* format_context_raw = format_context.get();
+	if ( avformat_open_input( &format_context_raw, "", nullptr, nullptr ) < 0 )
 	{
 		return std::unexpected( idhan::ModuleError( "Failed to open file" ) );
 	}
+	// avformat_open_input may reallocate the context, update our unique_ptr
+	format_context.release();
+	format_context.reset( format_context_raw );
 
-	if ( avformat_find_stream_info( format_context, nullptr ) < 0 )
+	if ( avformat_find_stream_info( format_context.get(), nullptr ) < 0 )
 	{
 		return std::unexpected( idhan::ModuleError( "Failed to find stream info" ) );
 	}
@@ -105,7 +186,7 @@ std::expected< idhan::ThumbnailerModuleI::ThumbnailInfo, idhan::ModuleError > FF
 		format_context->duration, AV_TIME_BASE_Q, format_context->streams[ video_stream_index ]->time_base ) };
 	const auto target_timestamp { static_cast< int64_t >( duration * 0.10 ) };
 
-	if ( av_seek_frame( format_context, video_stream_index, target_timestamp, AVSEEK_FLAG_BACKWARD ) < 0 )
+	if ( av_seek_frame( format_context.get(), video_stream_index, target_timestamp, AVSEEK_FLAG_BACKWARD ) < 0 )
 	{
 		return std::unexpected( idhan::ModuleError( "Failed to seek to timestamp" ) );
 	}
@@ -117,51 +198,57 @@ std::expected< idhan::ThumbnailerModuleI::ThumbnailInfo, idhan::ModuleError > FF
 		return std::unexpected( idhan::ModuleError( "Codec not found" ) );
 	}
 
-	AVCodecContext* codec_context = avcodec_alloc_context3( codec );
-	if ( avcodec_parameters_to_context( codec_context, codec_params ) < 0 )
+	std::unique_ptr< AVCodecContext, AVCodecContextDeleter > codec_context( avcodec_alloc_context3( codec ) );
+	if ( !codec_context )
 	{
-		avcodec_free_context( &codec_context );
+		return std::unexpected( idhan::ModuleError( "Failed to allocate codec context" ) );
+	}
+
+	if ( avcodec_parameters_to_context( codec_context.get(), codec_params ) < 0 )
+	{
 		return std::unexpected( idhan::ModuleError( "Failed to copy codec params" ) );
 	}
 
-	if ( avcodec_open2( codec_context, codec, nullptr ) < 0 )
+	if ( avcodec_open2( codec_context.get(), codec, nullptr ) < 0 )
 	{
-		avcodec_free_context( &codec_context );
 		return std::unexpected( idhan::ModuleError( "Failed to open codec" ) );
 	}
 
 	// Read frames until we get a valid video frame
-	AVPacket* packet = av_packet_alloc();
-	AVFrame* frame = av_frame_alloc();
+	const std::unique_ptr< AVPacket, AVPacketDeleter > packet( av_packet_alloc() );
+	const std::unique_ptr< AVFrame, AVFrameDeleter > frame( av_frame_alloc() );
+
+	if ( !packet || !frame )
+	{
+		return std::unexpected( idhan::ModuleError( "Failed to allocate packet or frame" ) );
+	}
+
 	bool frame_decoded = false;
 
-	while ( av_read_frame( format_context, packet ) >= 0 )
+	while ( av_read_frame( format_context.get(), packet.get() ) >= 0 )
 	{
 		if ( packet->stream_index == video_stream_index )
 		{
-			if ( avcodec_send_packet( codec_context, packet ) >= 0 )
+			if ( avcodec_send_packet( codec_context.get(), packet.get() ) >= 0 )
 			{
-				if ( avcodec_receive_frame( codec_context, frame ) >= 0 )
+				if ( avcodec_receive_frame( codec_context.get(), frame.get() ) >= 0 )
 				{
 					frame_decoded = true;
-					av_packet_unref( packet );
+					av_packet_unref( packet.get() );
 					break;
 				}
 			}
 		}
-		av_packet_unref( packet );
+		av_packet_unref( packet.get() );
 	}
 
 	if ( !frame_decoded )
 	{
-		av_frame_free( &frame );
-		av_packet_free( &packet );
-		avcodec_free_context( &codec_context );
 		return std::unexpected( idhan::ModuleError( "Failed to decode frame" ) );
 	}
 
 	// Convert frame to RGB24 using swscale
-	SwsContext* sws_context = sws_getContext(
+	std::unique_ptr< SwsContext, SwsContextDeleter > sws_context( sws_getContext(
 		codec_context->width,
 		codec_context->height,
 		codec_context->pix_fmt,
@@ -171,24 +258,58 @@ std::expected< idhan::ThumbnailerModuleI::ThumbnailInfo, idhan::ModuleError > FF
 		SWS_BILINEAR,
 		nullptr,
 		nullptr,
-		nullptr );
+		nullptr ) );
 
-	AVFrame* rgb_frame = av_frame_alloc();
+	if ( !sws_context )
+	{
+		return std::unexpected( idhan::ModuleError( "Failed to create swscale context" ) );
+	}
+
+	std::unique_ptr< AVFrame, AVFrameDeleter > rgb_frame( av_frame_alloc() );
+	if ( !rgb_frame )
+	{
+		return std::unexpected( idhan::ModuleError( "Failed to allocate RGB frame" ) );
+	}
+
 	rgb_frame->format = AV_PIX_FMT_RGB24;
 	rgb_frame->width = codec_context->width;
 	rgb_frame->height = codec_context->height;
-	av_frame_get_buffer( rgb_frame, 0 );
+
+	if ( av_frame_get_buffer( rgb_frame.get(), 0 ) < 0 )
+	{
+		return std::unexpected( idhan::ModuleError( "Failed to allocate RGB frame buffer" ) );
+	}
 
 	sws_scale(
-		sws_context, frame->data, frame->linesize, 0, codec_context->height, rgb_frame->data, rgb_frame->linesize );
+		sws_context.get(),
+		frame->data,
+		frame->linesize,
+		0,
+		codec_context->height,
+		rgb_frame->data,
+		rgb_frame->linesize );
 
-	VipsImage* image { vips_image_new_from_memory(
-		rgb_frame->data[ 0 ],
-		rgb_frame->linesize[ 0 ] * rgb_frame->height,
-		rgb_frame->width,
-		rgb_frame->height,
-		3,
-		VIPS_FORMAT_UCHAR ) };
+	// Create packed buffer without alignment padding
+	const size_t packed_line_size = rgb_frame->width * 3;
+	const size_t packed_size = packed_line_size * rgb_frame->height;
+	std::vector< unsigned char > packed_data( packed_size );
+
+	// Copy data line by line to remove padding
+	for ( int y = 0; y < rgb_frame->height; ++y )
+	{
+		std::memcpy(
+			packed_data.data() + ( y * packed_line_size ),
+			rgb_frame->data[ 0 ] + ( y * rgb_frame->linesize[ 0 ] ),
+			packed_line_size );
+	}
+
+	std::unique_ptr< VipsImage, VipsImageDeleter > image( vips_image_new_from_memory(
+		packed_data.data(), packed_size, rgb_frame->width, rgb_frame->height, 3, VIPS_FORMAT_UCHAR ) );
+
+	if ( !image )
+	{
+		return std::unexpected( idhan::ModuleError { "Failed to create VIPS image" } );
+	}
 
 	const float source_aspect { static_cast< float >( rgb_frame->width ) / static_cast< float >( rgb_frame->height ) };
 	const float target_aspect { static_cast< float >( width ) / static_cast< float >( height ) };
@@ -200,36 +321,26 @@ std::expected< idhan::ThumbnailerModuleI::ThumbnailInfo, idhan::ModuleError > FF
 
 	VipsImage* resized { nullptr };
 	if ( vips_resize(
-			 image,
+			 image.get(),
 			 &resized,
-			 static_cast< double >( width ) / static_cast< double >( vips_image_get_width( image ) ),
+			 static_cast< double >( width ) / static_cast< double >( vips_image_get_width( image.get() ) ),
 			 nullptr ) )
 	{
-		g_object_unref( image );
 		return std::unexpected( idhan::ModuleError { "Failed to resize image" } );
 	}
-	g_object_unref( image );
+	std::unique_ptr< VipsImage, VipsImageDeleter > resized_image( resized );
 
 	// Encode to PNG
 	void* output_buffer { nullptr };
 	std::size_t output_length { 0 };
-	if ( vips_pngsave_buffer( resized, &output_buffer, &output_length, nullptr ) )
+	if ( vips_pngsave_buffer( resized_image.get(), &output_buffer, &output_length, nullptr ) )
 	{
-		g_object_unref( resized );
-		return std::unexpected( idhan::ModuleError( "Failed to resize image" ) );
+		return std::unexpected( idhan::ModuleError( "Failed to encode PNG" ) );
 	}
-	g_object_unref( resized );
 
 	const std::vector< std::byte > output(
 		static_cast< std::byte* >( output_buffer ), static_cast< std::byte* >( output_buffer ) + output_length );
 	g_free( output_buffer );
-
-	// Cleanup
-	sws_freeContext( sws_context );
-	av_frame_free( &rgb_frame );
-	av_frame_free( &frame );
-	av_packet_free( &packet );
-	avcodec_free_context( &codec_context );
 
 	return idhan::ThumbnailerModuleI::ThumbnailInfo { .data = std::move( output ), .width = width, .height = height };
 }
