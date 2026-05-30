87db2acf0e1d4c4e172be054ec7afd1fe8019588
kj16609 <kj16609@dev.lolicon.zip>
2026-05-30 17:23:17 -0400

    Change EXECUTE to PERFORM in tag count trigger functions

--- IDHANMigration/src/93-tag_counts.sql
+++ IDHANMigration/src/93-tag_counts.sql
@@ -89,12 +89,12 @@ $$
 BEGIN
 
     CASE tg_op
-        WHEN 'INSERT' THEN EXECUTE add_count(new.tag_id, new.ideal_tag_id, new.tag_domain_id);
+        WHEN 'INSERT' THEN PERFORM add_count(new.tag_id, new.ideal_tag_id, new.tag_domain_id);
 
-        WHEN 'UPDATE' THEN EXECUTE remove_count(old.tag_id, old.ideal_tag_id, old.tag_domain_id);
-                           EXECUTE add_count(new.tag_id, new.ideal_tag_id, new.tag_domain_id);
+        WHEN 'UPDATE' THEN PERFORM remove_count(old.tag_id, old.ideal_tag_id, old.tag_domain_id);
+                           PERFORM add_count(new.tag_id, new.ideal_tag_id, new.tag_domain_id);
 
-        WHEN 'DELETE' THEN EXECUTE remove_count(old.tag_id, old.ideal_tag_id, old.tag_domain_id);
+        WHEN 'DELETE' THEN PERFORM remove_count(old.tag_id, old.ideal_tag_id, old.tag_domain_id);
 
         END CASE;
 
