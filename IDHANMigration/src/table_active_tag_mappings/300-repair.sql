6c7134b08b16237fb41803876f7aa8e8a9ec4db9
kj16609 <kj16609@dev.lolicon.zip>
2026-08-23 13:59:41 -0400

    Fixes repair using wrong col

--- IDHANMigration/src/table_active_tag_mappings/300-repair.sql
+++ IDHANMigration/src/table_active_tag_mappings/300-repair.sql
@@ -1,5 +1,5 @@
 UPDATE active_tag_mappings atm
-SET ideal_tag_id = (SELECT atm.effective_tag_id
+SET ideal_tag_id = (SELECT ta.effective_tag_id
                     FROM tag_aliases ta
                     WHERE atm.tag_id = ta.aliased_id
                       AND atm.tag_domain_id = ta.tag_domain_id);
